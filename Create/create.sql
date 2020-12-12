6387d371cd089c21ccd902170c284774a743d31a
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-11 19:12:45 -0600

    add create sql file

---
+++ Create/create.sql
@@ -0,0 +1,47 @@
+CREATE TABLE RAW_global_confirmed_cases(
+	"date*" DATE primary key,
+ 	"country*" VARCHAR2(50),
+	 "province*" VARCHAR2(50), 
+	 "lat" VARCHAR2(50), 
+	 "long" VARCHAR2(50), 
+	 "count" int
+);
+
+CREATE TABLE RAW_global_deaths(
+	"date*" DATE primary key,
+ 	"country*" VARCHAR2(50),
+	 "province*" VARCHAR2(50), 
+	 "lat" VARCHAR2(50), 
+	 "long" VARCHAR2(50), 
+	 "count" int
+);
+CREATE TABLE RAW_us_confirmed_cases(
+	"date*" varchar2(10) primary key,
+ 	"Province_State" VARCHAR2(50),
+	 "Admin2" VARCHAR2(50), 
+	 "UID" VARCHAR2(50), 
+	 "iso2" VARCHAR2(50), 
+	 "iso3" VARCHAR2(50), 
+	 "code3" VARCHAR2(50),
+	 "FIPS" VARCHAR2(50),
+	  "Country_Region" VARCHAR2(50),
+	 "lat" VARCHAR2(50), 
+	 "long_" VARCHAR2(50),
+	 "Combined_Key" VARCHAR2(50)
+);
+
+CREATE TABLE RAW_us_deaths(
+	"date*" varchar2(10) primary key,
+ 	"Province_State" VARCHAR2(50),
+	 "Admin2" VARCHAR2(50), 
+	 "UID" VARCHAR2(50), 
+	 "iso2" VARCHAR2(50), 
+	 "iso3" VARCHAR2(50), 
+	 "code3" VARCHAR2(50),
+	 "FIPS" VARCHAR2(50),
+	  "Country_Region" VARCHAR2(50),
+	 "lat" VARCHAR2(50), 
+	 "long_" VARCHAR2(50),
+	 "VARCHAR2(50)," VARCHAR2(50),
+	 "Population" int
+);
