69864bb8156129eb537303891c9e14097c878c40
David H <hauge343@umn.edu>
2020-12-12 20:19:39 -0600

    Adding to_date to dates of consecutive date prototype

--- Proto/setup_date_proto.sql
+++ Proto/setup_date_proto.sql
@@ -6,10 +6,10 @@ CREATE TABLE proto_consec_date(
   "count" int
 );
 
-INSERT INTO proto_consec_date  VALUES('31/01/20','blue',1);
-INSERT INTO proto_consec_date  VALUES('01/02/20','blue',2);
-INSERT INTO proto_consec_date  VALUES('02/02/20','blue',4);
-INSERT INTO proto_consec_date  VALUES('03/02/20','blue',8);
+INSERT INTO proto_consec_date  VALUES(TO_DATE('31/01/2020','DD/MM/YYYY'),'blue',1);
+INSERT INTO proto_consec_date  VALUES(TO_DATE('01/02/2020','DD/MM/YYYY'),'blue',2);
+INSERT INTO proto_consec_date  VALUES(TO_DATE('02/02/2020','DD/MM/YYYY'),'blue',4);
+INSERT INTO proto_consec_date  VALUES(TO_DATE('03/02/2020','DD/MM/YYYY'),'blue',8);
 
 set wrap off
 set line 160
