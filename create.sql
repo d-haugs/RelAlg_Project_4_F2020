60918aa9131d01aba5303ae3a1ded061bce2e555
David H <hauge343@umn.edu>
2020-12-12 22:45:56 -0600

    Remove quotes in create.sql. Formatting.

--- create.sql
+++ create.sql
@@ -1,19 +1,19 @@
 CREATE TABLE RAW_global_confirmed_cases(
-	"arbitraryID" VARCHAR2(32) primary key,
-	"date" DATE,
- 	"country" VARCHAR2(50),
-	 "province" VARCHAR2(50), 
-	 "lat" VARCHAR2(50), 
-	 "long" VARCHAR2(50), 
-	 "count" int
+	arbitraryID VARCHAR2(32) primary key,
+	arbdate DATE,
+ 	country VARCHAR2(50),
+	province VARCHAR2(50), 
+	lat VARCHAR2(50), 
+	long VARCHAR2(50), 
+	count int
 );
 
 CREATE TABLE RAW_global_deaths(
-	"arbitraryID" VARCHAR2(32) primary key,
-	"date" DATE,
- 	"country" VARCHAR2(50),
-	 "province" VARCHAR2(50), 
-	 "lat" VARCHAR2(50), 
-	 "long" VARCHAR2(50), 
-	 "count" int
+	arbitraryID VARCHAR2(32) primary key,
+	arbdate DATE,
+ 	country VARCHAR2(50),
+	province VARCHAR2(50), 
+	lat VARCHAR2(50), 
+	long VARCHAR2(50), 
+	count int
 );
