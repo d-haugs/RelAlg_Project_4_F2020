b7ba581f7df369453c780e9c5c56c44cc1cb0d13
David H <hauge343@umn.edu>
2020-12-12 21:19:03 -0600

    Attempting to add sys_guid to prototype table create and insert

--- Proto/setup_date_proto.sql
+++ Proto/setup_date_proto.sql
@@ -1,15 +1,16 @@
 
 
 CREATE TABLE proto_consec_date(
+  "arbitraryID" VARCHAR(32),
   "date*" DATE,
   "color*" VARCHAR2(50),
   "count" int
 );
 
-INSERT INTO proto_consec_date  VALUES(TO_DATE('31/01/2020','DD/MM/YYYY'),'blue',1);
-INSERT INTO proto_consec_date  VALUES(TO_DATE('01/02/2020','DD/MM/YYYY'),'blue',2);
-INSERT INTO proto_consec_date  VALUES(TO_DATE('02/02/2020','DD/MM/YYYY'),'blue',4);
-INSERT INTO proto_consec_date  VALUES(TO_DATE('03/02/2020','DD/MM/YYYY'),'blue',8);
+INSERT INTO proto_consec_date  VALUES(sys_guid(),TO_DATE('31/01/2020','DD/MM/YYYY'),'blue',1);
+INSERT INTO proto_consec_date  VALUES(sys_guid(),TO_DATE('01/02/2020','DD/MM/YYYY'),'blue',2);
+INSERT INTO proto_consec_date  VALUES(sys_guid(),TO_DATE('02/02/2020','DD/MM/YYYY'),'blue',4);
+INSERT INTO proto_consec_date  VALUES(sys_guid(),TO_DATE('03/02/2020','DD/MM/YYYY'),'blue',8);
 
 set wrap off
 set line 160
