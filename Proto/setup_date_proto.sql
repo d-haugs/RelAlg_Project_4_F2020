c4474af37469e4a2baa142307308130c4f7f1876
David H <hauge343@umn.edu>
2020-12-12 22:22:56 -0600

    Removing quotes from prototype create to match song list example's create.

--- Proto/setup_date_proto.sql
+++ Proto/setup_date_proto.sql
@@ -1,10 +1,10 @@
 
 
 CREATE TABLE proto_consec_date(
-  "arbitraryID" VARCHAR(32) primary key,
-  "date" DATE,
-  "color" VARCHAR2(50),
-  "count" int
+  arbitraryID VARCHAR(32) primary key,
+  arb_date DATE,
+  color VARCHAR2(50),
+  count int
 );
 
 INSERT INTO proto_consec_date  VALUES(sys_guid(),TO_DATE('31/01/2020','DD/MM/YYYY'),'blue',1);
