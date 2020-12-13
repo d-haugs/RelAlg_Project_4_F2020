40b109b672911987e3dc2c5e410110e2e5208441
David H <hauge343@umn.edu>
2020-12-12 22:35:15 -0600

    switch run over to group from mjoin (commented)

--- Proto/run_dp.sql
+++ Proto/run_dp.sql
@@ -4,12 +4,16 @@ SET SERVEROUTPUT ON
 DECLARE
 BEGIN
 
-ops.go(ops.mjoin_ra('a=proto_consec_date','b=proto_consec_date','color','color','p_mj_cdate'));
+-- ops.go(ops.mjoin_ra('a=proto_consec_date','b=proto_consec_date','color','color','p_mj_cdate'));
 
--- ops.go(ops.group_ra('color',))
+ops.go(ops.group_ra('proto_consec_date','color','count_sum=sum(count)','color_plus_count_sum'))
 
 END;
 /
 
 
 -- ops.go(ops.group_ra('songstream','song_id','total_streams=sum(number_of_streams)','id_of_song_with_total_streams'));
+
+select * from color_plus_count_sum;
+
+DROP TABLE color_plus_count_sum;
