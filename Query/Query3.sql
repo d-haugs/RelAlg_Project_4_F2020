b68b014297275bc281a5f7b55d3cd79bd973a901
David H <hauge343@umn.edu>
2020-12-13 23:48:53 -0600

    Added worst country per day

--- Query/Query3.sql
+++ Query/Query3.sql
@@ -5,7 +5,8 @@
 -- drop table raw_global_death_pair_non_province_countries;
 drop table RAW_global_deaths_cumul;
 drop table RAW_global_confirmed_cases_cumul;
-drop table matched_date_country_pair_w_case_and_death_data;
+drop table date_country_pair_w_ccase_and_death;
+drop table worst_country_per_day;
 
 
 SET SERVEROUTPUT ON
@@ -18,9 +19,10 @@ ops.go(ops.group_ra('RAW_global_deaths', 'arbdate,country','cumdeathCount=sum(de
 ops.go(ops.group_ra('RAW_global_confirmed_cases', 'arbdate,country', 'cumconfirmedCount=sum(confirmedCount)', 'RAW_global_confirmed_cases_cumul'));
 
 --match confirmed cases and deaths by same date,country
-ops.go(ops.mjoin_ra('RAW_global_deaths_cumul','RAW_global_confirmed_cases_cumul','arbdate,country','arbdate,country','matched_date_country_pair_w_case_and_death_data'));
+ops.go(ops.mjoin_ra('RAW_global_deaths_cumul','RAW_global_confirmed_cases_cumul','arbdate,country','arbdate,country','date_country_pair_w_ccase_and_death'));
 
--- ops.go(ops.reduce_ra('matched_date_country_pair_w_case_and_death_data','arbitraryID=TODO','arbdate,country,cumdeathCount,cumconfirmedCount','TODO'))
+--group over arbdate, carry country, func: max(death/case)
+ops.go(ops.group_ra('date_country_pair_w_ccase_and_death','arbdate','country','worst_death=max(cumdeathCount/cumconfirmedCount)','worst_country_per_day'))
 
 --!!!NOTE: do this later
 -- Match deaths and confirmed cases with their previous day data per date,country
