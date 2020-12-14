6afac33f0aefb2867acf63fda46cb15cb5868919
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-13 18:36:30 -0600

    update query2

--- Query/query2.sql
+++ Query/query2.sql
@@ -7,7 +7,7 @@ BEGIN
 -- ops.go(ops.project_ra('RAW_global_deaths',ops.allbut('lat,longitude'),'RAW_global_deaths_without_lat_longitude'));
 -- ops.go(ops.project_ra('RAW_global_confirmed_cases',ops.allbut('lat,longitude'),'RAW_global_confirmed_cases_without_lat_longitude'));
 ops.go(ops.project_ra('RAW_global_deaths','arbitraryID,arbdate,country,province,deathCount','RAW_global_deaths_without_lat_longitude'));
-ops.go(ops.project_ra('RAW_global_confirmed_cases''arbitraryID,arbdate,country,province,confirmedCount','RAW_global_confirmed_cases_without_lat_longitude'));
+ops.go(ops.project_ra('RAW_global_confirmed_cases','arbitraryID,arbdate,country,province,confirmedCount','RAW_global_confirmed_cases_without_lat_longitude'));
 
 -- Match join 
 ops.go(ops.mjoin_ra('a=RAW_global_deaths_without_lat_longitude','b=RAW_global_confirmed_cases_without_lat_longitude','country,province,arbdate','country,province,arbdate','country,province,arbdate,confirmedCount,deathCount','raw_global_confirmed_death_pair')); 
