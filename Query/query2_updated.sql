94a9cb5d3383dab3e040df057bec4a8a1d7f27e0
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-14 16:26:50 -0600

    query2-updated file added

--- Query/query2_updated.sql
+++ Query/query2_updated.sql
@@ -14,10 +14,11 @@ ops.go(ops.group_ra('RAW_global_confirmed_cases','arbdate,country','confirmed_ca
 -- Match join 
 -- Match Join all song data to get back "Song with its total number of streams"
 ops.go(ops.mjoin_ra('a=confirmed_count_by_country_date_pair','b=death_count_by_country_date_pair','country,arbdate','country,arbdate','country,arbdate,death_case_count,confirmed_case_count','confirmeddeath_pair_for_country')); 
-
+ ops.go(ops.group_ra('confirmeddeath_pair_for_country','country','death_case_count,confirmed_case_count','new_Date=max(arbdate)','latest_date_confirmeddeath_pair'));
 
 
 END;
 /
-
-select * from confirmeddeath_pair_for_country where rownum <= 30;
+select count(*) from death_count_by_country_date_pair;
+select count(*) from confirmed_count_by_country_date_pair;
+select count(*) from confirmeddeath_pair_for_country;
