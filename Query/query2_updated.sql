7a825a7ac31a459b0e7c5a1178879ed286d5b763
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-14 17:03:13 -0600

    query2-updated file added

--- Query/query2_updated.sql
+++ Query/query2_updated.sql
@@ -12,10 +12,12 @@ ops.go(ops.group_ra('RAW_global_confirmed_cases','arbdate,country','confirmed_ca
 -- Match join 
 
 ops.go(ops.mjoin_ra('a=confirmed_count_by_country_date_pair','b=death_count_by_country_date_pair','country,arbdate','country,arbdate','country,arbdate,death_case_count,confirmed_case_count','confirmeddeath_pair_for_country')); 
+
+-- Group By country 
+ops.go(ops.group_ra('confirmeddeath_pair_for_country','country','new_Date=max(arbdate)','death_confirmed_pair'));
+
+--  Proejct to get death to confirmed ratio
 -- ops.go(ops.project_ra('confirmeddeath_pair_for_country','country,arbdate,deathToConfirmedRatio=death_case_count/confirmed_case_count','country_deathToConfirmed_ratio'));
-
-ops.go(ops.group_ra('confirmeddeath_pair_for_country','country','death_case_count,confirmed_case_count','new_Date=max(arbdate)','death_confirmed_pair'));
-
 
 END;
 /
