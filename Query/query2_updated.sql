369e824e2bb37f94b50a95b585e5a2b9e1519353
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-14 17:21:38 -0600

    query2-updated file added

--- Query/query2_updated.sql
+++ Query/query2_updated.sql
@@ -4,6 +4,7 @@ DROP TABLE confirmed_count_by_country_date_pair;
 DROP TABLE confirmeddeath_pair_for_country;
 DROP TABLE FILTER_CONFIRMEDDEATH_FOR_COUNTRY;
 DROP TABLE country_deathToConfirmed_ratio;
+DROP TABLE death_confirmed_pair;
 SET SERVEROUTPUT ON
 DECLARE
 BEGIN
@@ -20,7 +21,10 @@ ops.go(ops.filter_ra('confirmeddeath_pair_for_country','confirmed_case_count!=0'
 ops.go(ops.project_ra('filter_confirmeddeath_for_country','country,arbdate,deathToConfirmedRatio=death_case_count/confirmed_case_count','country_deathToConfirmed_ratio'));
 
 -- Group By country 
-ops.go(ops.group_ra('country_deathToConfirmed_ratio','country','deathToConfirmedRatio','new_Date=max(arbdate)','death_confirmed_pair'));
+ops.go(ops.group_ra('country_deathToConfirmed_ratio','country','new_Date=max(arbdate)','death_confirmed_pair'));
+
+-- MJ 
+ops.go(ops.mjoin_ra('a=country_deathToConfirmed_ratio','b=death_confirmed_pair','country,arbdate','country,new_Date','country,arbdate,deathToConfirmedRatio','final_confirmeddeath_ration')); 
 
 
 END;
