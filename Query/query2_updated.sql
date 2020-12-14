2510eb89c5f14447af2e95c4c7aa412455e3d7ff
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-14 16:19:32 -0600

    query2-updated file added

--- Query/query2_updated.sql
+++ Query/query2_updated.sql
@@ -1,7 +1,7 @@
 -- @@drop
-DROP TABLE death_cases_count_by_country_date_pair;
-DROP TABLE confirmed_cases_count_by_country_date_pair;
-DROP TABLE confirmed_death_pair_for_country;
+DROP TABLE death_count_by_country_date_pair;
+DROP TABLE confirmed_count_by_country_date_pair;
+DROP TABLE confirmeddeath_pair_for_country;
 SET SERVEROUTPUT ON
 DECLARE
 BEGIN
@@ -13,11 +13,11 @@ ops.go(ops.group_ra('RAW_global_deaths','arbdate,country','death_case_count=sum(
 ops.go(ops.group_ra('RAW_global_confirmed_cases','arbdate,country','confirmed_case_count=sum(confirmedCount)','confirmed_count_by_country_date_pair'));
 -- Match join 
 -- Match Join all song data to get back "Song with its total number of streams"
-ops.go(ops.mjoin_ra('a=confirmed_count_by_country_date_pair','b=death_count_by_country_date_pair','country,arbdate','country,arbdate','country,arbdate,death_case_count,confirmed_case_count','confirmdeath_pair_for_country')); 
+ops.go(ops.mjoin_ra('a=confirmed_count_by_country_date_pair','b=death_count_by_country_date_pair','country,arbdate','country,arbdate','country,arbdate,death_case_count,confirmed_case_count','confirmeddeath_pair_for_country')); 
 
 
 
 END;
 /
 
-select * from confirmed_death_pair_for_country where rownum <= 30;
+select * from confirmeddeath_pair_for_country where rownum <= 30;
