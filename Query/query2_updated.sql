03e56906a32da7f5062f974777f17600e80bd6fb
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-14 16:46:29 -0600

    query2-updated file added

--- Query/query2_updated.sql
+++ Query/query2_updated.sql
@@ -12,9 +12,9 @@ ops.go(ops.group_ra('RAW_global_confirmed_cases','arbdate,country','confirmed_ca
 -- Match join 
 
 ops.go(ops.mjoin_ra('a=confirmed_count_by_country_date_pair','b=death_count_by_country_date_pair','country,arbdate','country,arbdate','country,arbdate,death_case_count,confirmed_case_count','confirmeddeath_pair_for_country')); 
-ops.go(ops.project_ra('confirmeddeath_pair_for_country','country,arbdate,deathToConfirmedRatio=death_case_count/confirmed_case_count','country_deathToConfirmed_ratio'));
+-- ops.go(ops.project_ra('confirmeddeath_pair_for_country','country,arbdate,deathToConfirmedRatio=death_case_count/confirmed_case_count','country_deathToConfirmed_ratio'));
 
--- ops.go(ops.group_ra('confirmeddeath_pair_for_country','country','death_case_count,confirmed_case_count','new_Date=max(arbdate)','latest_date_confirmeddeath_pair'));
+ops.go(ops.group_ra('confirmeddeath_pair_for_country','country','death_case_count,confirmed_case_count','new_Date=max(arbdate)','latest_date_confirmeddeath_pair'));
 
 
 END;
