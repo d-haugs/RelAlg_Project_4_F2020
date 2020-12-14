3f5b0d4e4461e143f551526f795acb5e330534dd
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-14 16:15:37 -0600

    query2-updated file added

--- Query/query2_updated.sql
+++ Query/query2_updated.sql
@@ -8,12 +8,12 @@ BEGIN
 
 -- ops.go(ops.project_ra('RAW_global_deaths','arbitraryID,arbdate,country,province,deathCount','deaths_without_lat_longitude'));
 -- ops.go(ops.project_ra('RAW_global_confirmed_cases','arbitraryID,arbdate,country,province,confirmedCount','confirmed_cases_without_lat_longitude'));
-ops.go(ops.group_ra('RAW_global_deaths','arbdate,country','death_case_count=sum(deathCount)','death_cases_count_by_country_date_pair'));
+ops.go(ops.group_ra('RAW_global_deaths','arbdate,country','death_case_count=sum(deathCount)','death_count_by_country_date_pair'));
 
-ops.go(ops.group_ra('RAW_global_confirmed_cases','arbdate,country','confirmed_case_count=sum(confirmedCount)','confirmed_cases_count_by_country_date_pair'));
+ops.go(ops.group_ra('RAW_global_confirmed_cases','arbdate,country','confirmed_case_count=sum(confirmedCount)','confirmed_count_by_country_date_pair'));
 -- Match join 
 -- Match Join all song data to get back "Song with its total number of streams"
-ops.go(ops.mjoin_ra('a=confirmed_cases_count_by_country_date_pair','b=death_cases_count_by_country_date_pair','country,arbdate','country,arbdate','country,arbdate,death_case_count,confirmed_case_count','confirmed_death_pair_for_country')); 
+ops.go(ops.mjoin_ra('a=confirmed_cases_count_by_country_date_pair','b=death_cases_count_by_country_date_pair','country,arbdate','country,arbdate','country,arbdate,death_case_count,confirmed_case_count','confirmdeath_pair_for_country')); 
 
 
 
