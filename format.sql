f1ed03af945c6e827416ffaf4a292111c57d4370
suvarna vellanki <suvarnavellanki@suvarnas-MacBook-Pro.local>
2020-12-14 19:34:53 -0600

    update format

--- format.sql
+++ format.sql
@@ -1,2 +1,10 @@
 set wrap off
 set line 160
+col arbitraryID    format a40;
+col arbdate        format a20;
+col country        format a20;
+col province       format a20;
+col lat            format a20;
+col longitude      format a20;
+
+
