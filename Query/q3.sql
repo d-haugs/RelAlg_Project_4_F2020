34f5e6abaa7c68ef3b2a0baa301d2bfeaa13fc04
David H <hauge343@umn.edu>
2020-12-14 20:34:09 -0600

    Adding comments to q3 query chain

--- Query/q3.sql
+++ Query/q3.sql
@@ -18,36 +18,38 @@ drop table worst_ratio_per_date;
 drop table days_count_as_worst_country;
 drop table most_days_as_worst_country;
 
---test group
--- drop table jan_feb_deaths_cumul;
-
 
 SET SERVEROUTPUT ON
 DECLARE
 BEGIN
 
-
+-- Generate RAW_deaths_per_country
 ops.go(ops.group_ra('RAW_global_deaths', 'arbdate,country','cumdeathCount=sum(deathCount)', 'cumul_deaths'));
+-- Generate RAW_confirmed_cases_per_country
 ops.go(ops.group_ra('RAW_global_confirmed_cases', 'arbdate,country', 'cumconfirmedCount=sum(confirmedCount)', 'cumul_cases'));
 
 --match confirmed cases and deaths by same date,country
 --WARNING: this is too big for the full dataset. must be broken down to run on that.
+--Generate Matched date-country pair with confirmed cases and deaths
 ops.go(ops.mjoin_ra('d=cumul_deaths','c=cumul_cases','arbdate,country','arbdate,country','short_name'));
 
---does not work
--- execute immediate 'select table_name from user_tables';
-
+--Generate date and previous date matched date-country pair with confirmed cases and deaths
 ops.go(ops.mjoin_ra('a=short_name','b=short_name','country,arbdate-1','country,arbdate','day_previousday_pair'));
 
 -- ======================================
 -- NOTE: following RA operations are written on good faith that the preceding operations have run despite backend problems with an earlier match join
 -- ======================================
+-- Generate date and previous date matched date-country pair with confirmed cases and deaths with 'a_' 'b_' dates removed
 ops.go(ops.reduce_ra('day_previousday_pair','arbdate=a_arbdate,country','a_cumdeathCount,b_cumdeathCount,a_cumconfirmedCount,b_cumconfirmedCount','single_date_day_previousday_pair'));
-ops.go(ops.project_ra('prev_day_match','arbitraryid,arb_date,color,count_change=a_count-b_count','prev_day_difference'));
+-- Generate Daily cases and deaths of date-country pair
 ops.go(ops.project_ra('single_date_day_previousday_pair','arbdate,country,day_deaths=a_cumdeathCount-b_cumdeathCount,day_cases=a_cumconfirmedCount-b_cumconfirmedCount','daily_case_death_datecountry_pair'));
+-- Generate Daily cases and deaths of date-country pair without case=0
 ops.go(ops.filter_ra('daily_case_death_datecountry_pair','day_cases!=0','daily_data_without_0_cases'));
+-- Generate Worst ratio Per date with country
 ops.go(ops.group_ra('daily_data_without_0_cases','arbdate','country','worst_ratio=max(day_death/day_cases)','worst_ratio_per_date'));
+-- Generate Day count as worst country
 ops.go(ops.group_ra('worst_ratio_per_date','country','country_count=count(*)','days_count_as_worst_country'));
+-- Generate Most Days as worst ratio of country
 ops.go(ops.group_ra('days_count_as_worst_country','country','most_days=max(country_count)','most_days_as_worst_country'));
 
 
