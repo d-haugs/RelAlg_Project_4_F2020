0f264165489e241cecb4fefc0ac29b4fae38550b
whitn152 <whitn152@umn.edu>
2020-12-17 21:00:19 -0600

    query 1 works!

--- Query/Query1.sql
+++ Query/Query1.sql
@@ -1,6 +1,15 @@
 --@create;
 --@Insert/DENORMALIZED_RAW_global_deaths_insert
 
+drop table raw_global_death_no_lat_long;
+drop table global_death_by_country;
+drop table global_death_by_country_1;
+drop table global_daily_death_by_country;
+drop table r_global_daily_death_by_country;
+drop table daily_death_count_by_country;
+drop table worst_daily_death_count_by_country;
+drop table worst_daily_death_count_by_country_with_date;
+
 SET SERVEROUTPUT ON
 DECLARE
 BEGIN
@@ -11,14 +20,16 @@ ops.go(ops.project_ra('RAW_global_deaths','country,province,arbdate,deathCount,a
 --Group by country to sum possible province data
 ops.go(ops.group_ra('raw_global_death_no_lat_long','country,arbdate','country_death_count=sum(deathCount)','global_death_by_country'));
 
+ops.go(ops.project_ra('global_death_by_country','country,arbdate=arbdate-1,country_death_count','global_death_by_country_1'));
+
 --match join global death with itself on country and date-1 to get consecutive day counts
-ops.go(ops.mjoin_ra('a=global_death_by_country','b=global_death_by_country','country,arbdate','country,arbdate-1','global_daily_death_by_country'));
+ops.go(ops.mjoin_ra('a=global_death_by_country','b=global_death_by_country_1','country,arbdate','country,arbdate','global_daily_death_by_country'));
 
 --reduce identifier to only need the later date
-ops.go(ops.reduce_ra('global_daily_death_by_country','country,arbdate=b_arbdate','b_country_death_count,a_country_death_count','r_global_daily_death_by_country'));
+--ops.go(ops.reduce_ra('global_daily_death_by_country','country,arbdate=b_arbdate','b_country_death_count,a_country_death_count','r_global_daily_death_by_country'));
 
 --project the counts as the differences of each other
-ops.go(ops.project_ra('r_global_daily_death_by_country','country,arbdate,country_daily_death_count = b_country_death_count - a_country_death_count','daily_death_count_by_country'));
+ops.go(ops.project_ra('global_daily_death_by_country','country,arbdate,country_daily_death_count = b_country_death_count - a_country_death_count','daily_death_count_by_country'));
 
 --find worst death total for a singular day for a country
 ops.go(ops.group_ra('daily_death_count_by_country','country','worst_count=max(country_daily_death_count)','worst_daily_death_count_by_country'));
@@ -29,11 +40,5 @@ ops.go(ops.mjoin_ra('o=daily_death_count_by_country','w=worst_daily_death_count_
 END;
 /
 
-drop table raw_global_death_no_lat_long;
-drop table global_death_by_country;
-drop table global_daily_death_by_country;
-drop table r_global_daily_death_by_country;
-drop table daily_death_count_by_country;
-drop table worst_daily_death_count_by_country;
+
 select * from worst_daily_death_count_by_country_with_date where rownum <= 30;
-drop table worst_daily_death_count_by_country_with_date;
